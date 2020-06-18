423a80885c42a07d8fb47bd4c49050c554d7e19c
timBurgum <66672633+timBurgum@users.noreply.github.com>
2020-06-18 14:27:45 +0100

    Filters and timer created and coded

--- MCQApp/mcqApp.js
+++ MCQApp/mcqApp.js
@@ -1,20 +1,24 @@
 // start button buttonClicked
-document.querySelectorAll(".randBTN")[0].addEventListener("click", play);
+//document.querySelectorAll(".randBTN")[0].addEventListener("click", play);
 document.querySelectorAll(".startBTN")[0].addEventListener("click", playMultiples);
+//document.querySelectorAll(".testBTN")[0].addEventListener("click", filterTopic);
 var scoreTag = document.querySelectorAll("h2")[0];
 var currentQID;
 var currentQ;
 var qNumber = 0;
 var playerScore = 0;
 var runningGame = false;
+var timerOn;
+var timeBar = document.querySelectorAll(".timer")[0];
 var picture = document.querySelectorAll("img")[0];
 console.log(picture);
 function play()
 {
   //gen a random number up to qArray length
+  filterTopic();
+  console.log(qArray);
   var rand = Math.floor(Math.random()*qArray.length);
-  console.log(rand);
-  //set global var as that question iD
+    //set global var as that question iD
   currentQ = qArray[rand]
   currentQID = qArray[rand].iD;
   //Display picure for that question
@@ -24,20 +28,48 @@ function play()
 //*******************************************************************//
 function playMultiples()
 {
+
   qNumber = 0;
   playerScore = 0;
   gameQuestions = [];
   runningGame = true;
-  createQuestionArray();
+  //Get number of selected questions
+  var numSelector = document.getElementsByName("noOfQuestions")[0];
+  var num = numSelector.options[numSelector.selectedIndex].value;
+  //run filters - refactor
+  filterTopic();
+  filterPaper();
+  filterYear();
+  //Handle number of questions left in filter error
+  //if number of questions in qArray is less than sumber of Q selected then set new number to qArray lenghth and alert
+  if(qArray.length==0)
+  {
+    alert("There are no questions in the database matching your filter selection.");
+    //break all calulcations and reset page
+    location.reload();
+    return false;
+  }
+  else if(qArray.length<num)
+  {
+    num = qArray.length;
+    alert("There are "+qArray.length+" questions that match your filters. This number has now been set.");
+  }
+
+
+  createQuestionArray(num);
+  if(timerOn)
+  {
+    startTimer();
+  }
   nextQuestion();
 }
 var gameQuestions = [];
-function createQuestionArray()
+function createQuestionArray(n)
 {
   //Clear the array ready for a new set
 
   //Generate an array of random numbers
-  for (i=0;i<5;i++)
+  for (i=0;i<n;i++)
   {
     //check if number exists in array first
     var randGameQ = Math.floor(Math.random()*qArray.length);
@@ -50,8 +82,165 @@ function createQuestionArray()
   }
 
 }
-//*******************************************************************//
+//******************************FILTERS***********************//
+function filterTopic()
+{
+var selectedT = document.getElementsByName("topics")[0];
+var topicID = selectedT.options[selectedT.selectedIndex].value;
+if(topicID == "any")
+{
 
+}
+else
+{
+var idArray = [];
+var arrayOfTopicIndex = [];
+
+qArray.forEach(q=>idArray.push(q.topic));
+//create a new array of topic where the index represents the id
+//loop through new array and create array of index of each matching topic
+  for(i=idArray.length;i>0;i--)
+  {
+    if((idArray[i-1]==topicID))
+    {
+      arrayOfTopicIndex.push(i-1);
+    }
+  }
+// Create a new array of the question objects linked to those index values
+var newArray =[];
+//loop through new array
+arrayOfTopicIndex.forEach(que => newArray.push(qArray[que]));
+qArray = newArray;
+}
+}
+
+function filterPaper()
+{
+  var selectedP = document.getElementsByName("paper")[0];
+  var paperID = selectedP.options[selectedP.selectedIndex].value;
+  if(paperID == "any")
+  {
+
+  }
+  else
+  {
+  var idArray = [];
+  var arrayOfPaperIndex = [];
+
+  qArray.forEach(q=>idArray.push(q.year));
+  //create a new array of topic where the index represents the id
+  //loop through new array and create array of index of each matching topic
+    for(i=idArray.length;i>0;i--)
+    {
+      if((idArray[i-1]==paperID))
+      {
+        arrayOfPaperIndex.push(i-1);
+      }
+    }
+  // Create a new array of the question objects linked to those index values
+  var newArray =[];
+  //loop through new array
+  arrayOfPaperIndex.forEach(que => newArray.push(qArray[que]));
+  qArray = newArray;
+  }
+}
+
+function filterYear()
+{
+  var selectedY = document.getElementsByName("year")[0];
+  console.log(selectedY);
+  var yearID = selectedY.options[selectedY.selectedIndex].value;
+  console.log(yearID);
+  if(yearID == "any")
+  {
+
+  }
+  else
+  {
+  var idArray = [];
+  var arrayOfYearIndex = [];
+
+  qArray.forEach(q=>idArray.push(q.year));
+  //create a new array of topic where the index represents the id
+  //loop through new array and create array of index of each matching topic
+    for(i=idArray.length;i>0;i--)
+    {
+      if((idArray[i-1]==yearID))
+      {
+        arrayOfYearIndex.push(i-1);
+      }
+    }
+  // Create a new array of the question objects linked to those index values
+  var newArray =[];
+  //loop through new array
+  arrayOfYearIndex.forEach(que => newArray.push(qArray[que]));
+  qArray = newArray;
+  }
+}
+
+//************************ Timer functions ************************//
+//get timeBar getElement
+
+//get which time option is selected
+function whichTimeSelected()
+{
+  var which;
+  for(i=0;i<document.querySelectorAll(".radioBTN").length;i++)
+  {
+    if(document.querySelectorAll(".radioBTN")[i].checked)
+    {
+      which = document.querySelectorAll(".radioBTN")[i].getAttribute("value");
+    }
+
+  }
+    if(which!="noTimer")
+    {
+      timerOn = true;
+      setTimer(which);
+    }
+    else
+    {
+      //turn off timer
+      timerOn = false;
+    }
+
+}
+
+function setTimer(maxTime)
+{
+    timeBar.setAttribute("max",maxTime);
+    timeBar.setAttribute("value",maxTime);
+}
+function decrementTimer()
+{
+  //check if timer is on 0
+  if(timeBar.getAttribute("value")==0)
+  {
+    clearInterval(startTimer);
+    console.log("timer stopped");
+    //iff game is running call nextQuestion
+    isGameOver();
+
+  }
+  else
+  {
+    timeBar.setAttribute("value",timeBar.getAttribute("value")-1);
+  }
+
+
+}
+
+function startTimer()
+{
+  whichTimeSelected()
+  var startTimer = setInterval(decrementTimer,1000);
+  console.log("timer started");
+}
+function resetTimer()
+{
+  whichTimeSelected();
+  console.log("timer reset");
+}
 
 
 //************************* ANSWER BUTTONS ************************//
@@ -83,7 +272,14 @@ function checkAnswer()
   console.log(playerScore);
   //Update players score inn html
   scoreTag.innerHTML = "Player score: " + playerScore;
+  isGameOver();
   // Give next question if game  is running
+
+}
+function isGameOver()
+{
+  clearInterval(startTimer);
+
   if(qNumber == gameQuestions.length)
   {
     console.log("running game set to false");
@@ -91,6 +287,14 @@ function checkAnswer()
     // set qpic to completed screen
     var filePath = "CompletedRoundScreen.png";
     picture.setAttribute("src",filePath);
+    startTimer=false;
+    clearInterval(startTimer);
+
+
+
+    alert("Your score is " + playerScore)
+    location.reload();
+
   }
   if(runningGame == true)
   {
@@ -101,13 +305,15 @@ function checkAnswer()
 
 function nextQuestion()
 {
+  //Stop any timing errors
+
 //set questionPicture and current q for answer check
 currentQ = qArray[gameQuestions[qNumber]];
 var filePath = "QuestionImages/"+ qArray[gameQuestions[qNumber]].iD  +".png";
 picture.setAttribute("src",filePath);
 //inrement qNumber
   qNumber++;
-
+resetTimer()
 
 }
 //*******************************************************************//
--- MCQApp/questionData.js
+++ MCQApp/questionData.js
@@ -1,47 +1,65 @@
+var qArray = []; // Array to store question objects
+initialiseQuestionArray();
+
 function Question(iD, answer, topic, paper, year, keyWords, level) {
   this.iD = iD;
   this.answer = answer;
   this.topic = topic;
   this.paper = paper;
+  this.year = year;
   this.keyWords = keyWords;
   this.level = level;
-}
-var qArray = []; // Array to store question objects
-qArray.push(new Question(0,"c","workEnergyPower","H156/01",2016,"power watt units quantities","as"));
-qArray.push(new Question(1,"b","workEnergyPower","H156/01",2016,"power watt units quantities","as"));
-qArray.push(new Question(2,"c","waves","H156/01",2016,"polarising intensity malus","as"));
-qArray.push(new Question(3,"d","quantum","H156/01",2016,"diffraction electrons","as"));
-qArray.push(new Question(4,"b","waves","H156/01",2016,"electromagnetic wavelength","as"));
-qArray.push(new Question(5,"a","forcesAndMotion","H156/01",2016,"acceleration graph force momentum","as"));
-qArray.push(new Question(6,"b","forcesAndMotion","H156/01",2016,"triangle forces friction ramp","as"));
-qArray.push(new Question(7,"b","materialsSprings","H156/01",2016,"spring elastic ratio springs ratios","as"));
-qArray.push(new Question(8,"a","electricity","H156/01",2016,"potential divider circuit circuits","as"));
-qArray.push(new Question(9,"c","newtonsLawsMomentum","H156/01",2016,"conservation momentum kinetic","as"));
-qArray.push(new Question(10,"d","electricity","H156/01",2016,"current conventional emf","as"));
-qArray.push(new Question(11,"b","electricity","H156/01",2016,"current kirchhoff kirchhoffs kirchhoff's","as"));
-qArray.push(new Question(12,"c","electricity","H156/01",2016,"uncertainty","as"));
-qArray.push(new Question(13,"c","forcesAndMotion","H156/01",2016,"acceleration graph bounce velocity","as"));
-qArray.push(new Question(14,"c","quantum","H156/01",2016,"electron gun ratio ratios ratio's","as"));
-qArray.push(new Question(15,"a","quantum","H156/01",2016,"photon photons photoelectric","as"));
-qArray.push(new Question(16,"d","materialsSprings","H156/01",2016,"potential elastic ratio springs ratios guitar youngs","as"));
-qArray.push(new Question(17,"c","forcesAndMotion","H156/01",2016,"projectile kinetic energy","as"));
-qArray.push(new Question(18,"d","electricity","H156/01",2016,"power ratio ratio's ratio resitor parallel","as"));
-qArray.push(new Question(19,"b","waves","H156/01",2016,"refraction","as"));
-qArray.push(new Question(20,"a","waves","H556/01",2015,"frequency units","a2"));
-qArray.push(new Question(21,"d","materialsSprings","H556/01",2015,"uncertainty units","a2"));
-qArray.push(new Question(22,"c","forcesAndMotion","H556/01",2015,"suvat","a2"));
-qArray.push(new Question(23,"c","workEnergyPower","H556/01",2015,"power resolving","a2"));
-qArray.push(new Question(24,"b","materialsSprings","H556/01",2015,"young modulus elastic","a2"));
-qArray.push(new Question(25,"a","newtonsLawsMomentum","H556/01",2015,"upthrust archemedis acceleration","a2"));
-qArray.push(new Question(26,"c","materialsSprings","H556/01",2015,"upthrust archemedis acceleration","a2"));
-qArray.push(new Question(27,"a","newtonsLawsMomentum","H556/01",2015,"momentum 2d","a2"));
-qArray.push(new Question(28,"b","shmCircular","H556/01",2015,"shm","a2"));
-qArray.push(new Question(29,"ans","thermalPhysics","H556/01",2015,"latent heat","a2"));
-qArray.push(new Question(30,"b","gravitationalFields","H556/01",2015,"ratio ratios gravitational potential","a2"));
-qArray.push(new Question(31,"ans","thermalPhysics","H556/01",2015,"gas laws","a2"));
-qArray.push(new Question(32,"b","gravitationalFields","H556/01",2015,"ratio ratios weight","a2"));
-qArray.push(new Question(33,"b","shmCircular","H556/01",2015,"angular velocity","a2"));
-qArray.push(new Question(34,"b","shmCircular","H556/01",2015,"angular velocity","a2"));
-qArray.push(new Question(35,"d","quantum","H556/01",2015,"nuclear quantum broglie","a2"));
 
-console.log("completed question object upload");
+  function getTopic()
+  {
+    return this.topic;
+  }
+
+}
+
+function initialiseQuestionArray()
+{
+  qArray.push(new Question(0,"c","workEnergyPower","H156/01","2016","power watt units quantities","as"));
+  qArray.push(new Question(1,"b","workEnergyPower","H156/01","2016","power watt units quantities","as"));
+  qArray.push(new Question(2,"c","waves","H156/01","2016","polarising intensity malus","as"));
+  qArray.push(new Question(3,"d","quantum","H156/01","2016","diffraction electrons","as"));
+  qArray.push(new Question(4,"b","waves","H156/01","2016","electromagnetic wavelength","as"));
+  qArray.push(new Question(5,"a","forcesAndMotion","H156/01","2016","acceleration graph force momentum","as"));
+  qArray.push(new Question(6,"b","forcesAndMotion","H156/01","2016","triangle forces friction ramp","as"));
+  qArray.push(new Question(7,"b","materialsSprings","H156/01","2016","spring elastic ratio springs ratios","as"));
+  qArray.push(new Question(8,"a","electricity","H156/01","2016","potential divider circuit circuits","as"));
+  qArray.push(new Question(9,"c","newtonsLawsMomentum","H156/01","2016","conservation momentum kinetic","as"));
+  qArray.push(new Question(10,"d","electricity","H156/01","2016","current conventional emf","as"));
+  qArray.push(new Question(11,"b","electricity","H156/01","2016","current kirchhoff kirchhoffs kirchhoff's","as"));
+  qArray.push(new Question(12,"c","electricity","H156/01","2016","uncertainty","as"));
+  qArray.push(new Question(13,"c","forcesAndMotion","H156/01","2016","acceleration graph bounce velocity","as"));
+  qArray.push(new Question(14,"c","quantum","H156/01","2016","electron gun ratio ratios ratio's","as"));
+  qArray.push(new Question(15,"a","quantum","H156/01","2016","photon photons photoelectric","as"));
+  qArray.push(new Question(16,"d","materialsSprings","H156/01","2016","potential elastic ratio springs ratios guitar youngs","as"));
+  qArray.push(new Question(17,"c","forcesAndMotion","H156/01","2016","projectile kinetic energy","as"));
+  qArray.push(new Question(18,"d","electricity","H156/01","2016","power ratio ratio's ratio resitor parallel","as"));
+  qArray.push(new Question(19,"b","waves","H156/01","2016","refraction","as"));
+  qArray.push(new Question(20,"a","waves","H556/01","2015","frequency units","a2"));
+  qArray.push(new Question(21,"d","materialsSprings","H556/01","2015","uncertainty units","a2"));
+  qArray.push(new Question(22,"c","forcesAndMotion","H556/01","2015","suvat","a2"));
+  qArray.push(new Question(23,"c","workEnergyPower","H556/01","2015","power resolving","a2"));
+  qArray.push(new Question(24,"b","materialsSprings","H556/01","2015","young modulus elastic","a2"));
+  qArray.push(new Question(25,"a","newtonsLawsMomentum","H556/01","2015","upthrust archemedis acceleration","a2"));
+  qArray.push(new Question(26,"c","materialsSprings","H556/01","2015","upthrust archemedis acceleration","a2"));
+  qArray.push(new Question(27,"a","newtonsLawsMomentum","H556/01","2015","momentum 2d","a2"));
+  qArray.push(new Question(28,"b","shmCircular","H556/01","2015","shm","a2"));
+  qArray.push(new Question(29,"thermalPhysics","H556/01","2015","latent heat","a2",""));
+  qArray.push(new Question(30,"b","gravitationalFields","H556/01","2015","ratio ratios gravitational potential","a2"));
+  qArray.push(new Question(31,"thermalPhysics","H556/01","2015","gas laws","a2",""));
+  qArray.push(new Question(32,"b","gravitationalFields","H556/01","2015","ratio ratios weight","a2"));
+  qArray.push(new Question(33,"b","shmCircular","H556/01","2015","angular velocity","a2"));
+  qArray.push(new Question(34,"b","shmCircular","H556/01","2015","angular velocity","a2"));
+  qArray.push(new Question(35,"d","quantum","H556/01","2015","nuclear quantum broglie","a2"));
+  console.log("completed question object upload");
+}
+
+function resetQuestionArray()
+{
+  qArray=[];
+  initialiseQuestionArray();
+}
